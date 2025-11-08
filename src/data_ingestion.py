dd392de22d19654b27bc9bda09275d96eb6c91e4
Hasnain <jokerking00045@gmail.com>
2025-11-08 05:38:39 -0800

    after the params is setted and used, also dvclive is setted

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import logging
+import yaml
 #make directory
 log_dir= "./logs"
 os.makedirs(log_dir,exist_ok=True)
@@ -24,6 +25,23 @@ file_logger.setFormatter(formatter)
 # Adding Loggers to the logging object
 logger.addHandler(cons_log)
 logger.addHandler(file_logger)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_data(data_url: str)->pd.DataFrame:
@@ -73,7 +91,8 @@ def save_data(train_data:pd.DataFrame,test_data:pd.DataFrame,data_path:str)->Non
 
 def main():
     try:
-        test_size=0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
         df=load_data("https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/spam.csv")
         df=preprocess_data(df)
         train,test= train_test_split(df,test_size=test_size,random_state=2)
